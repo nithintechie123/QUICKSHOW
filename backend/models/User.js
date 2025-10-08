9b43f30fffed4aab92ffda17ca4abd37e9076361
Nithin Kumar Gorintala <121857100+nithintechie123@users.noreply.github.com>
2025-10-08 14:44:48 +0530

    server

---
+++ backend/models/User.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+const userSchema=new mongoose.Schema({
+    _id:{type:String,required:true},
+    name:{type:String,required:true},
+    email:{type:String,required:true},
+})
