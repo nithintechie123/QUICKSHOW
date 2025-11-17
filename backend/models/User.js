661ef3669d69ddebf9745fa439e837b71ab9b52d
Nithin Kumar Gorintala <121857100+nithintechie123@users.noreply.github.com>
2025-11-17 21:07:43 +0530

    first commit

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,4 +4,9 @@ const userSchema=new mongoose.Schema({
     _id:{type:String,required:true},
     name:{type:String,required:true},
     email:{type:String,required:true},
+    image:{type:String,required:true}
 })
+
+const User=mongoose.model("User",userSchema);
+
+export default User;
